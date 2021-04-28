903eae6de4f1165cae5d9c7b66a183dbfe40162a
Bjarte Stien Karlsen <bjarte.karlsen@skatteetaten.no>
2021-04-28 21:02:29 +0200

    started on integrating flow cli

--- examples/demo/main.go
+++ examples/demo/main.go
@@ -3,24 +3,72 @@ package main
 import (
 	"log"
 
-	"github.com/bjartek/go-with-the-flow/gwtf"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/onflow/flow-cli/pkg/flowcli/config"
+	"github.com/onflow/flow-cli/pkg/flowcli/gateway"
+	"github.com/onflow/flow-cli/pkg/flowcli/output"
+	"github.com/onflow/flow-cli/pkg/flowcli/project"
+	"github.com/onflow/flow-cli/pkg/flowcli/services"
+	"github.com/onflow/flow-go-sdk/crypto"
 )
 
+func ca(flowConfigName string, accountName string) {
+
+	saAccountName := "emulator-account"
+	p, err := project.Load([]string{flowConfigName})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	logger := output.NewStdoutLogger(output.DebugLog)
+	gateway, err := gateway.NewGrpcGateway(config.DefaultEmulatorNetwork().Host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	service := services.NewServices(gateway, p, logger)
+
+	//This feels realy cumbersome just to get a Public key from a private key in config
+	key := p.AccountByName(accountName).DefaultKey().ToConfig()
+	pk := key.Context[config.PrivateKeyField]
+	privateKey, err := crypto.DecodePrivateKeyHex(crypto.ECDSA_P256, pk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	publicKey := privateKey.PublicKey().String()
+
+	a, err := service.Accounts.Create(
+		saAccountName,
+		[]string{publicKey},
+		[]int{1000},
+		key.SigAlgo.String(),
+		key.HashAlgo.String(),
+		[]string{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	spew.Dump(a)
+
+}
 func main() {
 
-	g := gwtf.
-		NewGoWithTheFlowEmulator().
-		CreateAccountPrintEvents("first", "second")
+	ca("flow.json", "first")
+	/*
+		g := gwtf.
+			NewGoWithTheFlowEmulator().
+			CreateAccountPrintEvents("first", "second")
 
-	g.TransactionFromFile("create_nft_collection").SignProposeAndPayAs("first").RunPrintEventsFull()
-	g.TransactionFromFile("arguments").SignProposeAndPayAs("first").StringArgument("argument1").RunPrintEventsFull()
-	g.TransactionFromFile("argumentsWithAccount").SignProposeAndPayAs("first").AccountArgument("second").RunPrintEventsFull()
-	g.TransactionFromFile("signWithMultipleAccounts").SignProposeAndPayAs("first").PayloadSigner("second").RunPrintEventsFull()
+		g.TransactionFromFile("create_nft_collection").SignProposeAndPayAs("first").RunPrintEventsFull()
+		g.TransactionFromFile("arguments").SignProposeAndPayAs("first").StringArgument("argument1").RunPrintEventsFull()
+		g.TransactionFromFile("argumentsWithAccount").SignProposeAndPayAs("first").AccountArgument("second").RunPrintEventsFull()
+		g.TransactionFromFile("signWithMultipleAccounts").SignProposeAndPayAs("first").PayloadSigner("second").RunPrintEventsFull()
 
-	g.ScriptFromFile("test").AccountArgument("second").Run()
+		g.ScriptFromFile("test").AccountArgument("second").Run()
 
-	//Run script that returns
-	result := g.ScriptFromFile("test").AccountArgument("second").RunReturns()
-	log.Printf("Script returned %s", result)
+		//Run script that returns
+		result := g.ScriptFromFile("test").AccountArgument("second").RunReturns()
+		log.Printf("Script returned %s", result)
+	*/
 
 }
--- go.mod
+++ go.mod
@@ -8,6 +8,7 @@ require (
 	github.com/enescakir/emoji v1.0.0
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/onflow/cadence v0.14.4
+	github.com/onflow/flow-cli v0.18.0
 	github.com/onflow/flow-go-sdk v0.17.0
 	github.com/pkg/errors v0.9.1
 	google.golang.org/grpc v1.33.0
