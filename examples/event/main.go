bd9cf5a27b2da51dbea092c85fb2bff5a060c80e
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2021-05-04 22:29:45 +0200

    versus event test

--- examples/event/main.go
+++ examples/event/main.go
@@ -2,28 +2,57 @@ package main
 
 import (
 	"fmt"
+	"sort"
+	"strconv"
 
 	"github.com/bjartek/go-with-the-flow/gwtf"
 )
 
 func main() {
 
-	g := gwtf.NewGoWithTheFlowEmulator()
+	g := gwtf.NewGoWithTheFlowMainNet()
 
 	eventsFetcher := g.EventFetcher().
-		Last(2).
-		EventIgnoringFields("flow.AccountCreated", []string{"codeHash"}).
-		EventIgnoringFields("flow.AccountKeyAdded", []string{"publicKey"})
-
-	g.CreateAccountPrintEvents("first")
+		Start(13978126).
+		End(14013459).
+		Event("A.d796ff17107bbff6.Versus.Bid")
 
 	events, err := eventsFetcher.Run()
 	if err != nil {
 		panic(err)
 	}
 
+	bid := make(map[string]float64)
 	fmt.Printf("%v", events)
+	for _, ev := range events {
+		fields := ev.Fields
 
+		price, err := strconv.ParseFloat(fields["price"], 64)
+		if err != nil {
+			panic(err)
+		}
+		if val, ok := bid[fields["bidder"]]; ok {
+			bid[fields["bidder"]] = val + price
+		} else {
+			bid[fields["bidder"]] = price
+		}
+	}
+	type kv struct {
+		Key   string
+		Value float64
+	}
+
+	var ss []kv
+	for k, v := range bid {
+		ss = append(ss, kv{k, v})
+	}
+	sort.Slice(ss, func(i, j int) bool {
+		return ss[i].Value > ss[j].Value
+	})
+
+	for _, kv := range ss {
+		fmt.Printf("%s, %v\n", kv.Key, kv.Value)
+	}
 	/*
 		//if you add an eventHook in discord to the flow.json in the example folder you can use this to send the events to a discord channel
 		msg, err := eventsFetcher.SendEventsToWebhook("gwtf")
