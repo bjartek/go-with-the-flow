a0580bb1d5f0e864a85cc3b605097f7e7b275add
Jay <976739120@qq.com>
2021-07-28 16:38:41 +0800

    fix

--- gwtf/cadence_test.go
+++ gwtf/cadence_test.go
@@ -7,33 +7,34 @@ import (
 )
 
 func TestCadenceValueToJsonString(t *testing.T) {
+
 	t.Run("Empty optional should be empty string", func(t *testing.T) {
 		value :=CadenceValueToJsonString(cadence.NewOptional(nil))
 		assert.Equal(t, `""`, value)
 	})
-
 	t.Run("Unwrap optional", func(t *testing.T) {
 		value :=CadenceValueToJsonString(cadence.NewOptional(cadence.NewString("foo")))
 		assert.Equal(t, `"foo"`, value)
 	})
-
 	t.Run("Array", func(t *testing.T) {
 		value :=CadenceValueToJsonString(cadence.NewArray([]cadence.Value { cadence.NewString("foo"), cadence.NewString("bar")}))
 		assert.Equal(t, `[
-			"foo",
-			"bar"
-		]`, value)
+    "foo",
+    "bar"
+]`, value)
 	})
 
 	t.Run("Dictionary", func(t *testing.T) {
 		dict := cadence.NewDictionary([]cadence.KeyValuePair{ { Key: cadence.NewString("foo"), Value: cadence.NewString("bar")}})
 		value :=CadenceValueToJsonString(dict)
 		assert.Equal(t, `{
-			"foo": "bar"
-		}`, value)
+    "foo": "bar"
+}`, value)
 	})
 
 	t.Run("Struct", func(t *testing.T) {
+
+
 		s := cadence.Struct{
 			Fields: []cadence.Value{cadence.NewString("bar")},
 			StructType: &cadence.StructType{
@@ -45,7 +46,8 @@ func TestCadenceValueToJsonString(t *testing.T) {
 		}
 		value :=CadenceValueToJsonString(s)
 		assert.Equal(t, `{
-			"foo": "bar"
-		}`, value)
+    "foo": "bar"
+}`, value)
 	})
+
 }
