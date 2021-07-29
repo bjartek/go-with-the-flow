0192116290a9c831c7232c2eb7435240caf13af7
Bjarte Karlsen <bjarte.karlsen@skatteetaten.no>
2021-07-29 03:00:02 +0200

    assert that events are sorted

--- examples/event_integration_test.go
+++ examples/event_integration_test.go
@@ -40,6 +40,32 @@ func TestEvents(t *testing.T) {
 		assert.Equal(t, 1, len(ev))
 	})
 
+	t.Run("Fetch last events and sort them ", func(t *testing.T) {
+		g := gwtf.NewTestingEmulator()
+		g.TransactionFromFile("mint_tokens").
+			SignProposeAndPayAsService().
+			AccountArgument("first").
+			UFix64Argument("100.0").
+			Test(t).
+			AssertSuccess().
+			AssertEventCount(3)
+
+		g.TransactionFromFile("mint_tokens").
+			SignProposeAndPayAsService().
+			AccountArgument("first").
+			UFix64Argument("100.0").
+			Test(t).
+			AssertSuccess().
+			AssertEventCount(3)
+
+		ev, err := g.EventFetcher().Last(3).Event("A.0ae53cb6e3f42a79.FlowToken.TokensMinted").Run()
+		assert.NoError(t, err)
+		assert.Equal(t, 2, len(ev))
+		assert.True(t, ev[0].BlockHeight < ev[1].BlockHeight)
+	})
+
+
+
 	t.Run("Fetch last write progress file", func(t *testing.T) {
 		g := gwtf.NewTestingEmulator()
 		g.TransactionFromFile("mint_tokens").
