1cf218a6f20249a488bbe076406b2d52246e84cb
Louis Guitton <louisguitton93@gmail.com>
2021-11-30 19:20:11 +0100

    Fix order of arguments in asserts

--- examples/script_integration_test.go
+++ examples/script_integration_test.go
@@ -12,12 +12,12 @@ func TestScript(t *testing.T) {
 
 	t.Run("Raw account argument", func(t *testing.T) {
 		value := g.ScriptFromFile("test").RawAccountArgument("0x1cf0e2f2f715450").RunReturnsInterface()
-		assert.Equal(t, value, "0x1cf0e2f2f715450")
+		assert.Equal(t, "0x1cf0e2f2f715450", value)
 	})
 
 	t.Run("Raw account argument", func(t *testing.T) {
 		value := g.ScriptFromFile("test").AccountArgument("first").RunReturnsInterface()
-		assert.Equal(t, value, "0x1cf0e2f2f715450")
+		assert.Equal(t, "0x1cf0e2f2f715450", value)
 	})
 
 	t.Run("Script should report failure", func(t *testing.T) {
